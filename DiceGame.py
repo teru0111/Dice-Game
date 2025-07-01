d054cf028858865898334e9d5cf1bf69a5f26374
teru0111 <11080111haruto@gmail.com>
2025-07-01 22:12:19 +0900

    Dice game: initial revision

---
+++ DiceGame.py
@@ -0,0 +1,12 @@
+import random
+import random as rd
+
+print('Rolling dice...')
+
+dice1 = random.randint(1,6)
+dice2 = random.randint(1,6)
+print("Die 1: "+str(dice1))
+print("Die 2: "+str(dice2))
+
+total = dice1+dice2
+print("Total value: "+str(total))
